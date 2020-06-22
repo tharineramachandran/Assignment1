35cd8900b3e4d080097f8796cdc130efd662fa74
Andre <andrecwj.18@gmail.com>
2020-06-23 03:17:22 +0800

    Extra features

---
+++ BonusTask_Extras/Controllers/NotesController.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using BonusTask_Extras.Models;
+using Firebase.Database;
+using Firebase.Database.Query;
+
+namespace BonusTask_Extras.Controllers
+{
+    public class NotesController : Controller
+    {
+        // GET: Notes/Create
+        public ActionResult AddNotes()
+        {
+            return View();
+        }
+
+        // POST: Notes/Create
+        [HttpPost]
+        public async System.Threading.Tasks.Task<ActionResult> AddNotesAsync(NotesModel notesModel)
+        {
+            var firebaseClient = new FirebaseClient("https://fir-notes-df65c.firebaseio.com/");
+
+            try
+            {
+                string title = notesModel.Title;
+                string name = notesModel.Note;
+
+                var result = await firebaseClient
+                    .Child("Notes")
+                    .Child(title)
+                    .PostAsync(name);
+
+                return View();
+            }
+            catch
+            {
+                return View();
+            }
+        }
+    }
+}
---
+++ BonusTask_Extras/Models/NotesModel.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace BonusTask_Extras.Models
+{
+    public class NotesModel
+    {
+        public string Title { get; set; }
+        public string Note { get; set; }
+    }
+}
