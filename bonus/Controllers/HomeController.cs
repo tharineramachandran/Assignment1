a31e24bad902f30e68a91d3a2f4d3616d7de373c
tharine ramachandran <54879683+tharineramachandran@users.noreply.github.com>
2020-06-24 22:21:01 +0800

    mood tracker

--- bonus/Controllers/HomeController.cs
+++ bonus/Controllers/HomeController.cs
@@ -5,6 +5,13 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using ParallelDots;
+using DeepAI;
+using System.IO;
+using Newtonsoft.Json.Linq;
+using System.Threading;
+using System.Drawing;
+using System.Windows.Forms;
 
 namespace bonus.Controllers
 {
@@ -16,7 +23,12 @@ namespace bonus.Controllers
 
             return View();
         }
-        
+        public ActionResult Mood()
+        {
+            ViewBag.Title = "Mood Page";
+
+            return View();
+        }
         public async System.Threading.Tasks.Task<ActionResult> Addnotes()
         {
 
@@ -26,6 +38,28 @@ namespace bonus.Controllers
             {
                 string title = Request.Form["title"];
                 string name = Request.Form["note"];
+                var  file = Request.Files["image"];
+                DeepAI_API api = new DeepAI_API(apiKey: "7cc97bee-3a97-44bd-bb9c-a5cad72bc4df" );
+
+                StandardApiResponse resp = api.callStandardApi("facial-expression-recognition", new
+                {
+                    image = name,
+                });
+                Console.Write(api.objectAsJsonString(resp));
+
+ 
+
+                DeepAI_API apei = new DeepAI_API(apiKey: "7cc97bee-3a97-44bd-bb9c-a5cad72bc4df");
+                string path = Path.Combine(Server.MapPath("~/"),
+                                       Path.GetFileName(file.FileName));
+                file.SaveAs(path); 
+                StandardApiResponse rty = apei.callStandardApi("facial-expression-recognition", new
+                {
+                    image =  System.IO.File.OpenRead(path),
+            });
+                Console.Write(apei.objectAsJsonString(rty));
+
+
 
 
                 var result = await firebaseClient
@@ -38,6 +72,92 @@ namespace bonus.Controllers
             catch (Exception ex)
             {
                 return Json(ex);
+            }
+        }
+
+        public ActionResult Addmood()
+        {
+
+
+            try
+            {
+                var file = Request.Files["image"];
+                var timestamp = DateTime.Now.ToString("DD/MM/YY");
+                if (file != null)
+                {
+
+                    DeepAI_API apei = new DeepAI_API(apiKey: "7cc97bee-3a97-44bd-bb9c-a5cad72bc4df");
+                    string path = Path.Combine(Server.MapPath("~/"),
+                                           Path.GetFileName(file.FileName));
+                    if (!System.IO.File.Exists(path))
+                    {
+                        file.SaveAs(path);
+                    }
+                    var fileread = System.IO.File.OpenRead(path);
+                    StandardApiResponse rty = apei.callStandardApi("facial-expression-recognition", new
+                    {
+                        image = fileread,
+                    });
+                    var returnjson = apei.objectAsJsonString(rty);
+                    fileread.Close();
+                    /// Add Firebase that uploads the results 
+                    JObject json = JObject.Parse(returnjson);
+                    var moodsdetected = json["output"]["expressions"];
+                    if (moodsdetected.HasValues)
+                    {
+                        string bestmood = "";
+                        double bestconfidence = 0;
+                        foreach (var mood in moodsdetected)
+                        {
+                            var confidence = double.Parse(mood["confidence"].ToString());
+                            var emotion = mood["emotion"].ToString();
+                            if (confidence > bestconfidence)
+                            {
+
+                                bestconfidence = confidence;
+                                bestmood = emotion;
+                            }
+
+
+
+
+
+
+                        }
+
+
+                        Dispose();
+
+                        System.IO.File.Delete(path);
+                        /// Add Firebase that uploads the results  bestmood  n timestamp   
+                        /// 
+
+
+
+
+
+
+
+
+                        Response.StatusCode = 200;
+                        return Json("Successful");
+                    }else
+                    {
+                        Response.StatusCode = 400;
+                        return Json("Could not track your emotion from the image, Try uploading a different image ");
+
+                    }
+                }
+                else {
+                    Response.StatusCode = 400;
+                    return Json("Add an image");
+
+                }
+            }
+            catch (Exception ex)
+            {
+                Response.StatusCode = 400;
+                return Json("an error has occurred");
             }
         }
     }
